632e762f10061924f0d2dff551224cd6f690fc55
Akhil-star <akhilakki1305@gmail.com>
2020-10-12 07:39:29 +0530

    Weclome

---
+++ src/com/addressbookcollection/Address.java
@@ -0,0 +1,8 @@
+package com.addressbookcollection;
+
+public class Address {
+    public static void main(String[] args) {
+		System.out.println("welcome");
+		System.out.println("Contact created");
+	}
+}
